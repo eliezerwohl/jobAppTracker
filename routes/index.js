981a0079d855b80cee4b8aa167632eef9dd64b9e
Eliezer Wohl <eliezerwohl@gmail.com>
2017-02-10 15:30:40 -0500

    jsonSend

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ module.exports = function (app) {
 	app.get("/", function(req, res){
 		res.sendFile( process.cwd() + "/public/views/home.html")
 	})
+		app.post("/login", function(req, res){
+			debugger
+		
+	})
+
 		app.get("/main", function(req, res){
 		res.sendFile( process.cwd() + "/public/views/home.html")
 	})
